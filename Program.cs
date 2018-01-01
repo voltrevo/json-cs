5ae0634b363d657a981d762f6ffa5505ac9ae926
Andrew Morris <voltrevo@gmail.com>
2018-01-02 10:34:39 +1100

    Add ToString

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ using System.Linq;
 
 namespace JsonParser
 {
-    abstract public class Value
+    public enum Type {StringMap, Array, String, Number};
+
+    public abstract class Value
     {
-        public enum Type {StringMap, Array, String, Number};
-        Type type;
+        public abstract Type GetJsonType();
 
         public static explicit operator Dictionary<string, Value>(Value value)
         {
@@ -47,30 +48,87 @@ namespace JsonParser
         {
             return ((List<Value>)value).ConvertAll(v => (double)v);
         }
+
+        protected abstract string ToStringImpl();
+
+        public string ToString() { return this.ToStringImpl(); }
     }
 
     public class StringValue : Value
     {
+        public override Type GetJsonType() { return Type.String; }
         public string data { get; set; }
         public StringValue(string data) { this.data = data; }
+
+        protected override string ToStringImpl()
+        {
+            // TODO: escaping
+            return $"\"{data}\"";
+        }
+
+        public static string ToStringExternal(string s)
+        {
+            return (new StringValue(s)).ToString();
+        }
     }
 
     public class NumberValue : Value
     {
+        public override Type GetJsonType() { return Type.Number; }
         public double data { get; set; }
         public NumberValue(double data) { this.data = data; }
+
+        protected override string ToStringImpl()
+        {
+            return this.data.ToString();
+        }
     }
 
     public class ArrayValue : Value
     {
+        public override Type GetJsonType() { return Type.Array; }
         public List<Value> data { get; set; }
         public ArrayValue(List<Value> data) { this.data = data; }
+
+        protected override string ToStringImpl()
+        {
+            string result = "[";
+            bool first = true;
+
+            foreach (Value v in this.data) {
+                if (!first) { result += ','; }
+                result += v.ToString();
+                first = false;
+            }
+
+            result += "]";
+
+            return result;
+        }
     }
 
     public class StringMapValue : Value
     {
+        public override Type GetJsonType() { return Type.StringMap; }
         public Dictionary<string, Value> data { get; set; }
         public StringMapValue(Dictionary<string, Value> data) { this.data = data; }
+
+        protected override string ToStringImpl()
+        {
+            string result = "{";
+            bool first = true;
+
+            foreach (var (key, value) in this.data)
+            {
+                if (!first) { result += ','; }
+                result += StringValue.ToStringExternal(key) + ':' + value.ToString();
+                first = false;
+            }
+
+            result += '}';
+
+            return result;
+        }
     }
 
     class Program
