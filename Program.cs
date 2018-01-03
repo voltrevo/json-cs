7c2655f2067b12665aa22bcae2fefdecf81bdad5
Andrew Morris <voltrevo@gmail.com>
2018-01-03 11:06:40 +1100

    Add FromString and mutation to example

--- Program.cs
+++ Program.cs
@@ -16,7 +16,16 @@ namespace Json
                 {"primes", new JsonArray() {2, 3, 5, 7, 11}},
             };
 
+            JsonValue v2 = JsonValue.FromString(@"{
+                ""point"": {""x"": 2.18, ""y"": -3.7},
+                ""squares"": [1, 4, 9, 16, 25]
+            }");
+
+            JsonArray squares = (JsonArray)v2["squares"];
+            squares.Add(36);
+
             Console.WriteLine(v.ToString("    "));
+            Console.WriteLine(v2.ToString("    "));
         }
     }
 }
